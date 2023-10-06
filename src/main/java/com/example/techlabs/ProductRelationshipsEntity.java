d921aed5bcd4306bc48ed26f6d6572a914dc09c9
ddooby.doobob <ddooby.doobob@kakaoenterprise.com>
2023-10-06 17:47:18 +0900

    feat: Column 설명 및 ProductEntity에 상품명 컬럼 추가

--- src/main/java/com/example/techlabs/ProductRelationshipsEntity.java
+++ src/main/java/com/example/techlabs/ProductRelationshipsEntity.java
@@ -1,11 +1,11 @@
 package com.example.techlabs;
-
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Comment;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 @Getter
 @Setter
@@ -19,16 +19,20 @@ public class ProductRelationshipsEntity {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "target_item_id", columnDefinition = "타겟 상품 ID", nullable = false)
+    @Comment("타겟 상품 ID")
+    @Column(name = "target_item_id", nullable = false)
     private Long targetItemId;
 
-    @Column(name = "result_item_id", columnDefinition = "연관된 상품 ID", nullable = false)
+    @Comment("연관된 상품 ID")
+    @Column(name = "result_item_id", nullable = false)
     private String resultItemId;
 
-    @Column(name = "score", columnDefinition = "연관도 점수", nullable = false)
+    @Comment("연관도 점수")
+    @Column(name = "score", nullable = false)
     private BigDecimal score;
 
-    @Column(name = "original_price", columnDefinition = "연관 상품 순위", nullable = false)
+    @Comment("연관 상품 순위")
+    @Column(name = "original_price", nullable = false)
     private BigDecimal originalPrice;
 
 }
