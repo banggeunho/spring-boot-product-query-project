0c06927adaedad2963f659adec8c5dbb70ba25c2
ddooby.doobob <ddooby.doobob@kakaoenterprise.com>
2023-10-11 18:57:45 +0900

    feat: CommonUtil 메소드 NPE 방어 코드 추가

--- src/main/java/com/example/techlabs/base/common/CommonUtil.java
+++ src/main/java/com/example/techlabs/base/common/CommonUtil.java
@@ -1,11 +1,15 @@
 package com.example.techlabs.base.common;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+@Component
 public class CommonUtil {
 
     public static  <T> T findByKey(List<T> list, Predicate<T> predicate) {
@@ -21,7 +25,17 @@ public class CommonUtil {
     }
 
     public static <T, R> List<R> extractKey(List<T> list, Function<T, R> function) {
+
+        if (list == null) {
+            throw new IllegalArgumentException("Input list cannot be null.");
+        }
+
+        if (function == null) {
+            throw new IllegalArgumentException("Key extraction function cannot be null.");
+        }
+
         return list.stream()
+                .filter(Objects::nonNull)
                 .map(function)
                 .collect(Collectors.toList());
     }
